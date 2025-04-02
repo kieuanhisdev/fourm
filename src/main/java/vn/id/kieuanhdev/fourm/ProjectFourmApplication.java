2fdcdb6d3cafd06402f6a7308edaba4aeff50332
KieuAnhDev <Kieuanh.dev@gmail.com>
2025-04-02 13:56:36 +0700

    start project

---
+++ src/main/java/vn/id/kieuanhdev/fourm/ProjectFourmApplication.java
@@ -0,0 +1,13 @@
+package vn.id.kieuanhdev.fourm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFourmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFourmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/id/kieuanhdev/fourm/ProjectFourmApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.kieuanhdev.fourm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFourmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
